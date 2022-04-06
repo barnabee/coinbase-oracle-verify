562f84b39400b593d7046ca19bc156e63474b21b
Barney Mannerings <b.mannerings@gmail.com>
2022-04-06 22:31:03 +0100

    Use stdin to make decoding easier to reuse

--- decode_cbdata.js
+++ decode_cbdata.js
@@ -1,12 +1,9 @@
 const Web3 = require('web3');
-
-// Load sample data
-const cbdata1 = require('./cbdata1.json');
-const cbdata2 = require('./cbdata2.json');
-const cbdata3 = require('./cbdata3.json');
+const fs = require('fs');
 
 /* Decodes ABI encoded Coinbase Oracle data and attempts to recover the public key and verify the signature
  *
+ * Try: `node decode_cbdata.js < cbdata1.json` OR `node --experimental-fetch fetch_cbdata.js | node decode_cbdata.js`
  */
 
 
@@ -17,24 +14,17 @@ async function decodeAndVerify(data) {
 	for (let i=0; i<messages.length; ++i) {
 
 		// Decode the data — this works just fine and we get extremely reasonable looking data
-		data = Object.values(web3.eth.abi.decodeParameters(['string', 'uint', 'string', 'uint'], messages[i])).slice(0, -1);
+		record = Object.values(web3.eth.abi.decodeParameters(['string', 'uint', 'string', 'uint'], messages[i])).slice(0, -1);
 
 		// Attempt to recover the signer, if this is 0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC we think it's "good"
 		adr = web3.eth.accounts.recover(web3.utils.keccak256(messages[i]), signatures[i], false); 
 		
 		// Print what we found...
-		console.log(data, adr === '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC' ? 'signature OK' : 'BAD signature!');
+		console.log(record, adr === '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC' ? 'signature OK' : 'BAD signature!');
 	}
 }
 
 
-// Print the outputs for some sample data (you can use fetch_cbdata.js to get your own if you have a Coinbase API key)
-
-console.log('cbdata1');
-decodeAndVerify(cbdata1);
-
-console.log('\ncbdata2');
-decodeAndVerify(cbdata2);
-
-console.log('\ncbdata3');
-decodeAndVerify(cbdata3);
+// Decide and verify the signature from standard input (expect JSON in Coinbase Oracle API format)
+const inputJson = JSON.parse(fs.readFileSync(0))
+decodeAndVerify(inputJson);
